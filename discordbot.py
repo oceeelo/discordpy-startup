1650e8c6ae9fcf5c47b4482913348c0d76ea99a0
oceeelo <80452339+oceeelo@users.noreply.github.com>
2021-03-11 20:57:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,17 +11,6 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
-@bot.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == 'neko':
-        await message.channel.send('にゃーん')    
-
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('にゃーん')
