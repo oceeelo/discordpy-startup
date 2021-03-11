a074f3bcae0495ee665e97c6acad8c1b4b1b7d0c
oceeelo <80452339+oceeelo@users.noreply.github.com>
2021-03-11 20:37:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,4 +1,5 @@
-from discord.ext import commands
+from discord.ext
+import commands
 import os
 import traceback
 import discord
@@ -7,11 +8,14 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
 
-# 起動時に動作する処理
 @client.event
-async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
 
 @bot.event
